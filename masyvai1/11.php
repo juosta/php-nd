d0f689cae1f0ed1e399fc517ab779d91b794131d
juosta <79445286+juosta@users.noreply.github.com>
2021-06-02 18:03:07 +0300

    11 uzd

--- masyvai1/11.php
+++ masyvai1/11.php
@@ -7,3 +7,29 @@
 // dalies sumas (neskaičiuojant vidurinės). Jeigu sumų skirtumas (modulis,
 // absoliutus dydis) yra didesnis nei | 30 | rūšiavimą kartokite. (Kad sumos
 // nesiskirtų viena nuo kitos daugiau nei per 30)
+$dydis = 101;
+$random1 = [];
+while(count($random1) < $dydis ){
+    array_push($random1, rand(0,300));
+    $random1 = array_unique($random1);
+}
+print_r($random1);
+$maxval = max($random1);
+$isrusiuotas =[];
+$isrusiuotas[$dydis/2] = $maxval;
+unset($random1[array_keys($random1, $maxval)[0]]);
+echo '<hr>';
+$middle = ceil($dydis/2);
+for($i = 0; $i < $middle-1; $i++)
+{
+    $maxval = max($random1);
+    $isrusiuotas[($dydis/2)+$i+1] = $maxval;
+    unset($random1[array_keys($random1, $maxval)[0]]);
+    if(!empty($random1)){
+    $maxval = max($random1);
+    $isrusiuotas[($dydis/2)-$i-1] = $maxval;
+    unset($random1[array_keys($random1, $maxval)[0]]);
+    }
+}
+print_r($isrusiuotas);
+echo '<br>';
