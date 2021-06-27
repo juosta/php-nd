581f1596a124faf9157a9fd3fc5bcffe40b42cb2
juosta <79445286+juosta@users.noreply.github.com>
2021-06-27 09:35:43 +0300

    obj 8 pataisytmas

--- objektiniai/8/Pinigine.php
+++ objektiniai/8/Pinigine.php
@@ -4,14 +4,18 @@ class Pinigine {
 
     private $popieriniaiPinigai;
     private $metaliniaiPinigai;
+    private $banknotai;
+    private $monetos;
 
     public function ideti($kiekis){
         if($kiekis<0){
             return;
         }
         if($kiekis <=2 ){
+            $this->monetos++;
             return $this->metaliniaiPinigai+=(double) $kiekis;
         } else {
+            $this->banknotai++;
             return $this->popieriniaiPinigai+=(double) $kiekis;
         }
     }
@@ -20,47 +24,59 @@ class Pinigine {
         echo $this->metaliniaiPinigai+$this->popieriniaiPinigai;
     }
 
-    public function monetos(){
-        $suma = ($this->metaliniaiPinigai+$this->popieriniaiPinigai%5)*100;
-        $monetos = 0;
-        _d($suma,"pradine suma");
-        $monetuArr = [200,100,50,20,10,5,2,1];
-        foreach($monetuArr as $moneta){
-            _d($moneta,"koks moneta");
-            if($suma/$moneta>=1){
-                $kiek = 0;
-                $kiek += (double) floor($suma / $moneta);
-                _d($kiek,"kiek yra $moneta");
-                $suma -= (int) $kiek *$moneta;
-                _d($suma,"kokia suma lieka");
-                $monetos += $kiek;
-            }
-        }
-        _d($suma,"suma, po visu skaiciavimu");
+    public function banknotai(){
 
-        return $monetos;
+        return $this->banknotai;
     }
+    public function monetos(){
+
+        return $this->monetos;
+    }
+
+
+    // public function monetos(){
+        // $suma = ($this->metaliniaiPinigai+$this->popieriniaiPinigai%5)*100;
+        // $monetos = 0;
+        // _d($suma,"pradine suma");
+        // $monetuArr = [200,100,50,20,10,5,2,1];
+        // foreach($monetuArr as $moneta){
+        //     _d($moneta,"koks moneta");
+        //     if($suma/$moneta>=1){
+        //         $kiek = 0;
+        //         $kiek += (int) floor($suma / $moneta);
+        //         _d($kiek,"kiek yra $moneta");
+        //         $suma -= (int) $kiek *$moneta;
+        //         _d($suma,"kokia suma lieka");
+        //         $monetos += $kiek;
+        //     }
+        // }
+        // _d($suma,"suma, po visu skaiciavimu");
+
+        // return $monetos;
+    // }
 
     
 
-    public function banknotai(){
-        $suma = $this->metaliniaiPinigai+$this->popieriniaiPinigai;
-        $banknotai = 0;
-        $banknotuArr = [500,200,100,50,20,10,5];
-        foreach($banknotuArr as $banknotas){
-            _d($banknotas,"koks banknotas");
-            if($suma/$banknotas>=1){
-                $kiek = 0;
-                $kiek += (double) floor($suma / $banknotas);
-                _d($kiek,"kiek yra $banknotas");
-                $suma -= (double) $kiek *$banknotas;
-                _d($suma,"kokia suma lieka");
-                $banknotai += $kiek;
-            }
-        }
-        _d($suma,"suma, po visu skaiciavimu");
+    // public function banknotai(){
+        // $suma = $this->metaliniaiPinigai+$this->popieriniaiPinigai;
+        // $banknotai = 0;
+        // $banknotuArr = [500,200,100,50,20,10,5];
+        // foreach($banknotuArr as $banknotas){
+        //     _d($banknotas,"koks banknotas");
+        //     if($suma/$banknotas>=1){
+        //         $kiek = 0;
+        //         $kiek += (int) floor($suma / $banknotas);
+        //         _d($kiek,"kiek yra $banknotas");
+        //         $suma -= (int) $kiek *$banknotas;
+        //         _d($suma,"kokia suma lieka");
+        //         $banknotai += $kiek;
+        //     }
+        // }
+        // _d($suma,"suma, po visu skaiciavimu");
 
-        return $banknotai;
-    }
+        // return $banknotai;
+    // }
 
+    
+    
 }
--- objektiniai/8/index.php
+++ objektiniai/8/index.php
@@ -3,13 +3,18 @@ require __DIR__ . '/Pinigine.php';
 $pinigine = new Pinigine;
 
 $pinigine->ideti(0.73);
-$pinigine->ideti(5300);
+$pinigine->ideti(100);
+$pinigine->ideti(25.33);
+$pinigine->ideti(30);
 
+echo "Suma pinigineje: <br>";
 $pinigine->skaiciuoti();
 echo '<br>';
 
+echo "Kiek banknotu pinigineje: <br>";
 echo $pinigine->banknotai();
 echo '<br>';
+echo "Kiek monetu pinigineje pinigineje: <br>";
 echo $pinigine->monetos();
 
 
