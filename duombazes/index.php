e2beb0a3d8aa05038361f755a4be7516afecc21d
juosta <79445286+juosta@users.noreply.github.com>
2021-07-08 22:59:34 +0300

    duombazes

--- duombazes/index.php
+++ duombazes/index.php
@@ -22,3 +22,20 @@ $pdo = new PDO($dsn, $user, $pass, $options);
 // } catch(PDOException $e) {
 //     echo 'ERROR: ' . $e->getMessage();
 // }
+
+// INSERT INTO table_name (column1, column2, column3,...)
+// VALUES (value1, value2, value3,...)
+
+$sql = "INSERT INTO users (name, surname, account_no, personal_no)
+VALUES ('Meta','Meskinija','LT77 5545 9004 0746 9949','51608078514')";
+
+
+$pdo->query($sql);
+
+
+$sql = "SELECT id, name, surname, account_no, personal_no, balance FROM users";
+$stmt = $pdo->query($sql); //DB steitmentas
+while ($row = $stmt->fetch()) // duoda viena eilute
+{
+    echo $row['id']. ' ' .$row['name']. ' ' .$row['surname']. ' ' .$row['account_no']. ' ' .$row['personal_no'] . ' ' .$row['balance'] . '<br>';
+}
